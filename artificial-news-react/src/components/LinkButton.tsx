26f452f5579b3c9633b466e34825af51dbcfd40e
Justin Martz <justinmartz@protonmail.com>
2025-06-28 15:17:56 -0600

    [ui] Stub out LinkButton component

---
+++ artificial-news-react/src/components/LinkButton.tsx
@@ -0,0 +1,5 @@
+const LinkButton = ({buttonText}: {buttonText: string}) => {
+    return <div className="border py-1 px-2 rounded-sm">{buttonText}</div>
+}
+
+export default LinkButton;
--- artificial-news-react/src/components/Pagination.tsx
+++ artificial-news-react/src/components/Pagination.tsx
@@ -1,5 +1,18 @@
+import LinkButton from "./LinkButton";
+
+const PaginationText = () => {
+    return <p>Showing 1 to 10 of 20 results</p>
+}
+
 const Pagination = () => {
-    return <div className="flex justify-between md:pb-3 md:pt-3 bg-blue-50">Showing 10 per page</div>
+    return <div className="flex justify-between md:pb-3 md:pt-3">
+        <PaginationText />
+        <div className="flex gap-x-3">
+        <LinkButton buttonText="Previous" />
+        <LinkButton buttonText="Next" />
+        </div>
+
+    </div>
 }
 
 export default Pagination;
