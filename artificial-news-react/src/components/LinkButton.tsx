da842134d005a3947ceb549011a0d124222c4d97
Justin Martz <justinmartz@protonmail.com>
2025-06-29 20:56:06 -0600

    [ui] Add link to welcome page for all articles

--- artificial-news-react/src/components/LinkButton.tsx
+++ artificial-news-react/src/components/LinkButton.tsx
@@ -1,23 +1,43 @@
+import { Link } from 'react-router';
+
 const LinkButton = ({
   buttonText,
   active,
   handleClick,
+  to,
 }: {
   buttonText: string;
   active: boolean;
-  handleClick: () => void;
+  handleClick?: () => void;
+  to?: string;
 }) => {
   return (
-    <button
-    onClick={handleClick}
-      className={
-        active
-          ? 'border border-gray-200 bg-green-100 py-1 px-2 rounded-sm cursor-pointer'
-          : 'border border-gray-200 py-1 px-2 rounded-sm'
-      }
-    >
-      {buttonText}
-    </button>
+    <>
+      {to ? (
+        <Link to={to}>
+          <button
+            className={
+              active
+                ? 'border border-gray-200 bg-cyan-300 py-1 px-2 rounded-sm cursor-pointer'
+                : 'border border-gray-200 py-1 px-2 rounded-sm'
+            }
+          >
+            {buttonText}
+          </button>
+        </Link>
+      ) : (
+        <button
+          onClick={handleClick}
+          className={
+            active
+              ? 'border border-gray-200 bg-cyan-300 py-1 px-2 rounded-sm cursor-pointer'
+              : 'border border-gray-200 py-1 px-2 rounded-sm'
+          }
+        >
+          {buttonText}
+        </button>
+      )}
+    </>
   );
 };
 
