6a67575c646ddfbea578bc1fbb09cdad01f48716
Justin Martz <justinmartz@protonmail.com>
2025-10-12 16:02:07 -0600

    DEFECT - Uncomment db save

--- artificial-news-spring-boot/src/main/java/dev/justinmartz/artificial_news/LoadDatabase.java
+++ artificial-news-spring-boot/src/main/java/dev/justinmartz/artificial_news/LoadDatabase.java
@@ -2,6 +2,8 @@ package dev.justinmartz.artificial_news;
 
 import dev.justinmartz.artificial_news.entities.Article;
 import dev.justinmartz.artificial_news.repositories.ArticleRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -9,56 +11,142 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Configuration
 @ConditionalOnProperty(name = "test-data.enabled", havingValue = "true")
 public class LoadDatabase {
-  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-  @Bean
-  CommandLineRunner initDatabase(ArticleRepository articleRepository) {
-    List<Article> testArticles = buildTestArticles();
-//    articleRepository.saveAll(testArticles);
+    @Bean
+    CommandLineRunner initDatabase(ArticleRepository articleRepository) {
+        List<Article> testArticles = buildTestArticles();
+            articleRepository.saveAll(testArticles);
 
-    return args -> {
-        for (Article article : testArticles) {
-            log.info("Preloading: {}", article.getId());
-        }
-    };
-  }
+        return args -> {
+            for (Article article : testArticles) {
+                log.info("Preloading: {}", article.getId());
+            }
+        };
+    }
 
-  private List<Article> buildTestArticles() {
-    Article article1 = new Article(), article2 = new Article(), article3 = new Article();
+    private List<Article> buildTestArticles() {
+        Article article1 = new Article(), article2 = new Article(), article3 = new Article();
 
-    article1.setCreatedAt(LocalDateTime.parse("2025-06-08T08:16:54.519613"));
-    article1.setDateline("June 8, 2025 • 8:16 AM");
-    article1.setHeadline("Giraffes Take to the Streets in Seattle's Most Whimsical Marathon Yet");
-    article1.setAuthor("Naomi Fernandez");
-    article1.setAuthorPhoto("Naomi-Fernandez-1749392212194.png");
-    article1.setArticleBody(
-        "In a delightful twist on the traditional marathon, the streets of Seattle played host to the first-ever Whimsical Giraffe Marathon over the weekend. Participants, dressed in eye-catching giraffe costumes complete with long necks and spotted ensembles, ran through the city's most iconic routes, attracting cheering crowds and curious onlookers. The event, which took place on Saturday, aimed to combine fitness with fun while raising awareness for wildlife conservation efforts. \\\"It's not just about running; it's about making a statement and bringing joy,\\\" said event organizer Lisa Thompson.\\n\\nThe marathon began at the crack of dawn at the famous Seattle Center, weaving through the bustling Pike Place Market, and culminating at the serene shores of Green Lake. Competitors, ranging from serious athletes to families with young children, embraced the whimsy of the day with gusto. The race was not timed, encouraging participants to enjoy the journey rather than rush to the finish line. \\\"I came with my kids, and they absolutely loved it,\\\" shared local resident, Mark Johnson, who participated with his family. \\\"It's a fantastic way to engage the community and support a good cause.\\\"\\n\\nBeyond the entertainment value, the Whimsical Giraffe Marathon served a deeper purpose. A portion of the proceeds was directed towards giraffe conservation projects in Africa, underscoring the plight of these gentle giants whose populations have been dwindling due to habitat loss and poaching. The event highlighted the importance of international cooperation in wildlife preservation and drew attention to Seattle's commitment to global conservation efforts. As the day concluded with a festive atmosphere at the finish line, complete with music and refreshments, Thompson was optimistic about growing the event in years to come. \\\"This is just the beginning of a new tradition in Seattle,\\\" she exclaimed, her eyes gleaming with hope.");
-    article1.setArticlePhoto("Giraffes-Take-to-the-Streets-in-Seattle's-Most-Whimsical-Marathon-Yet-1749392214339.png");
+        article1.setCreatedAt(LocalDateTime.parse("2025-06-08T08:16:54.519613"));
+        article1.setDateline("June 8, 2025 • 8:16 AM");
+        article1.setHeadline(
+                "Giraffes Take to the Streets in Seattle's Most Whimsical Marathon Yet");
+        article1.setAuthor("Naomi Fernandez");
+        article1.setAuthorPhoto("Naomi-Fernandez-1749392212194.png");
+        article1.setArticleBody(
+                "In a delightful twist on the traditional marathon, the streets of Seattle played"
+                    + " host to the first-ever Whimsical Giraffe Marathon over the weekend."
+                    + " Participants, dressed in eye-catching giraffe costumes complete with long"
+                    + " necks and spotted ensembles, ran through the city's most iconic routes,"
+                    + " attracting cheering crowds and curious onlookers. The event, which took"
+                    + " place on Saturday, aimed to combine fitness with fun while raising"
+                    + " awareness for wildlife conservation efforts. \\\"It's not just about"
+                    + " running; it's about making a statement and bringing joy,\\\" said event"
+                    + " organizer Lisa Thompson.\\n"
+                    + "\\n"
+                    + "The marathon began at the crack of dawn at the famous Seattle Center,"
+                    + " weaving through the bustling Pike Place Market, and culminating at the"
+                    + " serene shores of Green Lake. Competitors, ranging from serious athletes to"
+                    + " families with young children, embraced the whimsy of the day with gusto."
+                    + " The race was not timed, encouraging participants to enjoy the journey"
+                    + " rather than rush to the finish line. \\\"I came with my kids, and they"
+                    + " absolutely loved it,\\\" shared local resident, Mark Johnson, who"
+                    + " participated with his family. \\\"It's a fantastic way to engage the"
+                    + " community and support a good cause.\\\"\\n"
+                    + "\\n"
+                    + "Beyond the entertainment value, the Whimsical Giraffe Marathon served a"
+                    + " deeper purpose. A portion of the proceeds was directed towards giraffe"
+                    + " conservation projects in Africa, underscoring the plight of these gentle"
+                    + " giants whose populations have been dwindling due to habitat loss and"
+                    + " poaching. The event highlighted the importance of international cooperation"
+                    + " in wildlife preservation and drew attention to Seattle's commitment to"
+                    + " global conservation efforts. As the day concluded with a festive atmosphere"
+                    + " at the finish line, complete with music and refreshments, Thompson was"
+                    + " optimistic about growing the event in years to come. \\\"This is just the"
+                    + " beginning of a new tradition in Seattle,\\\" she exclaimed, her eyes"
+                    + " gleaming with hope.");
+        article1.setArticlePhoto(
+                "Giraffes-Take-to-the-Streets-in-Seattle's-Most-Whimsical-Marathon-Yet-1749392214339.png");
 
-    article2.setCreatedAt(LocalDateTime.parse("2025-06-07T08:38:39.969026"));
-    article2.setDateline("June 7, 2025 • 8:38 AM");
-    article2.setHeadline("Ghostly Delights Await at Denver's Haunted Cupcake Festival");
-    article2.setAuthor("Samantha Lin");
-    article2.setAuthorPhoto("Samantha-Lin-1749393518091.png");
-    article2.setArticleBody(
-        "This October, Denver will play host to a spine-tingling affair that promises to be as sweet as it is spectral: the Haunted Cupcake Festival. Set against the backdrop of the city's historic Larimer Square, the festival will kick off on October 14th, just in time to get everyone into the Halloween spirit. Organized by the local baking enthusiasts of the Denver Sweet Society, the event invites both locals and tourists to indulge in a delectable array of cupcakes while experiencing a few spooky thrills along the way.\\n\\nAmong the many attractions, the festival will feature a haunted maze crafted entirely out of cupcake-themed decor, providing a unique twist on the traditional haunted house experience. \\\"We wanted to create something fun and whimsical that would appeal to all ages,\\\" said Marissa Caldwell, event coordinator and founder of the Denver Sweet Society. \\\"Our cupcakes will be dressed up in their Halloween best, from ghoulish ghosts to decadent dark chocolate bats.\\\" Visitors can expect an impressive variety of flavors, including caramel apple cobweb, black velvet, and even pumpkin spice specter.\\n\\nThe festival hopes to draw a large crowd thanks to its combination of culinary creativity and festive fun. All proceeds from the event will benefit the Children’s Hospital Colorado, ensuring that the kitchens of Denver's bakers will not only whip up delightful desserts, but also serve a good cause. \\\"It's incredible to see how food can bring people together for such a meaningful purpose,\\\" Caldwell added. As the festival date approaches, anticipation is building for what promises to be an unforgettable celebration of both the spooky and the sweet.");
-    article2.setArticlePhoto("Ghostly-Delights-Await-at-Denver's-Haunted-Cupcake-Festival-1749393519798.png");
+        article2.setCreatedAt(LocalDateTime.parse("2025-06-07T08:38:39.969026"));
+        article2.setDateline("June 7, 2025 • 8:38 AM");
+        article2.setHeadline("Ghostly Delights Await at Denver's Haunted Cupcake Festival");
+        article2.setAuthor("Samantha Lin");
+        article2.setAuthorPhoto("Samantha-Lin-1749393518091.png");
+        article2.setArticleBody(
+                "This October, Denver will play host to a spine-tingling affair that promises to be"
+                    + " as sweet as it is spectral: the Haunted Cupcake Festival. Set against the"
+                    + " backdrop of the city's historic Larimer Square, the festival will kick off"
+                    + " on October 14th, just in time to get everyone into the Halloween spirit."
+                    + " Organized by the local baking enthusiasts of the Denver Sweet Society, the"
+                    + " event invites both locals and tourists to indulge in a delectable array of"
+                    + " cupcakes while experiencing a few spooky thrills along the way.\\n"
+                    + "\\n"
+                    + "Among the many attractions, the festival will feature a haunted maze crafted"
+                    + " entirely out of cupcake-themed decor, providing a unique twist on the"
+                    + " traditional haunted house experience. \\\"We wanted to create something fun"
+                    + " and whimsical that would appeal to all ages,\\\" said Marissa Caldwell,"
+                    + " event coordinator and founder of the Denver Sweet Society. \\\"Our cupcakes"
+                    + " will be dressed up in their Halloween best, from ghoulish ghosts to"
+                    + " decadent dark chocolate bats.\\\" Visitors can expect an impressive variety"
+                    + " of flavors, including caramel apple cobweb, black velvet, and even pumpkin"
+                    + " spice specter.\\n"
+                    + "\\n"
+                    + "The festival hopes to draw a large crowd thanks to its combination of"
+                    + " culinary creativity and festive fun. All proceeds from the event will"
+                    + " benefit the Children’s Hospital Colorado, ensuring that the kitchens of"
+                    + " Denver's bakers will not only whip up delightful desserts, but also serve a"
+                    + " good cause. \\\"It's incredible to see how food can bring people together"
+                    + " for such a meaningful purpose,\\\" Caldwell added. As the festival date"
+                    + " approaches, anticipation is building for what promises to be an"
+                    + " unforgettable celebration of both the spooky and the sweet.");
+        article2.setArticlePhoto(
+                "Ghostly-Delights-Await-at-Denver's-Haunted-Cupcake-Festival-1749393519798.png");
 
-    article3.setCreatedAt(LocalDateTime.parse("2025-06-06T08:43:26.153504"));
-    article3.setDateline("June 6, 2025 • 8:43 AM");
-    article3.setHeadline("Mystical Quilts Draw Bidders to Savannah's Eerie Auction");
-    article3.setAuthor("Lila Ellis");
-    article3.setAuthorPhoto("Lila-Ellis-1749393805761.png");
-    article3.setArticleBody(
-        "In the heart of Savannah, Georgia, where history intertwines with mysticism, an unusual auction drew crowds to the city’s historic district this past weekend. The Ghostly Quilt Auction, hosted at the renowned Davenport House Museum, featured an array of handcrafted quilts said to be imbued with ethereal qualities. Each quilt carries a touch of Savannah's storied past, reportedly stitched by hands long since gone, and according to local lore, under the supervision of benevolent spirits. The event held on the eve of Halloween added an extra layer of intrigue, inviting both skeptics and believers alike to bid on these textile pieces of history.\\n\\nThe quilts, painstakingly curated by local historian Margaret Jenkins, are believed to possess supernatural attributes, with patterns that occasionally change ever so subtly under moonlight. \\\"These quilts are more than just fabric and thread,\\\" Jenkins explained to attendees, her voice echoing through the halls once walked by figures of the 19th century. \\\"They tell the stories of the past, whispers sewn into each stitch.\\\" The auction attracted collectors nationwide who sought to own not just a piece of art, but a fragment of an otherworldly narrative that ties them to the city's vibrant and often mysterious history.\\n\\nDespite the spectral claims, the event served as a boon for Savannah’s tourism, drawing attendees from all over the U.S. while spotlighting the city's rich tapestry of folklore. John Michaels, a noted folklorist at the University of Georgia, expressed admiration for how the event highlighted Savannah’s unique blend of history and mystery. \\\"This auction demonstrates the enduring allure of Savannah's past,\\\" Michaels noted. \\\"It's a city that never forgets its ghosts and celebrates the stories they have left behind.\\\" With all proceeds going to the preservation of local historical sites, the Ghostly Quilt Auction not only captivated imaginations but also contributed to the safeguarding of Savannah's heritage.");
-    article3.setArticlePhoto("Mystical-Quilts-Draw-Bidders-to-Savannah's-Eerie-Auction-1749393804690.png");
+        article3.setCreatedAt(LocalDateTime.parse("2025-06-06T08:43:26.153504"));
+        article3.setDateline("June 6, 2025 • 8:43 AM");
+        article3.setHeadline("Mystical Quilts Draw Bidders to Savannah's Eerie Auction");
+        article3.setAuthor("Lila Ellis");
+        article3.setAuthorPhoto("Lila-Ellis-1749393805761.png");
+        article3.setArticleBody(
+                "In the heart of Savannah, Georgia, where history intertwines with mysticism, an"
+                    + " unusual auction drew crowds to the city’s historic district this past"
+                    + " weekend. The Ghostly Quilt Auction, hosted at the renowned Davenport House"
+                    + " Museum, featured an array of handcrafted quilts said to be imbued with"
+                    + " ethereal qualities. Each quilt carries a touch of Savannah's storied past,"
+                    + " reportedly stitched by hands long since gone, and according to local lore,"
+                    + " under the supervision of benevolent spirits. The event held on the eve of"
+                    + " Halloween added an extra layer of intrigue, inviting both skeptics and"
+                    + " believers alike to bid on these textile pieces of history.\\n"
+                    + "\\n"
+                    + "The quilts, painstakingly curated by local historian Margaret Jenkins, are"
+                    + " believed to possess supernatural attributes, with patterns that"
+                    + " occasionally change ever so subtly under moonlight. \\\"These quilts are"
+                    + " more than just fabric and thread,\\\" Jenkins explained to attendees, her"
+                    + " voice echoing through the halls once walked by figures of the 19th century."
+                    + " \\\"They tell the stories of the past, whispers sewn into each stitch.\\\""
+                    + " The auction attracted collectors nationwide who sought to own not just a"
+                    + " piece of art, but a fragment of an otherworldly narrative that ties them to"
+                    + " the city's vibrant and often mysterious history.\\n"
+                    + "\\n"
+                    + "Despite the spectral claims, the event served as a boon for Savannah’s"
+                    + " tourism, drawing attendees from all over the U.S. while spotlighting the"
+                    + " city's rich tapestry of folklore. John Michaels, a noted folklorist at the"
+                    + " University of Georgia, expressed admiration for how the event highlighted"
+                    + " Savannah’s unique blend of history and mystery. \\\"This auction"
+                    + " demonstrates the enduring allure of Savannah's past,\\\" Michaels noted."
+                    + " \\\"It's a city that never forgets its ghosts and celebrates the stories"
+                    + " they have left behind.\\\" With all proceeds going to the preservation of"
+                    + " local historical sites, the Ghostly Quilt Auction not only captivated"
+                    + " imaginations but also contributed to the safeguarding of Savannah's"
+                    + " heritage.");
+        article3.setArticlePhoto(
+                "Mystical-Quilts-Draw-Bidders-to-Savannah's-Eerie-Auction-1749393804690.png");
 
-    return List.of(article1, article2, article3);
-  }
+        return List.of(article1, article2, article3);
+    }
 }
