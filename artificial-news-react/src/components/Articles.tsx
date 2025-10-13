1024a0774e70389ca56cf15a0fddc7d25e23b911
Justin Martz <justinmartz@protonmail.com>
2025-10-13 13:41:00 -0600

    [ui] Fix width of bottom borders

--- artificial-news-react/src/components/Articles.tsx
+++ artificial-news-react/src/components/Articles.tsx
@@ -21,16 +21,22 @@ export default function Articles() {
   return (
     <main className="bg-white flex flex-grow md:flex-grow-0 lg:h-[75dvh] w-11/12 md:w-4/5 rounded-md shadow-md">
       <article className="flex flex-col justify-between w-full md:p-6 md:h-full">
-        <div className="overflow-y-auto">
-          {pagedArticlesResult.isLoading
-            ? Array(3)
+        <div className="overflow-y-auto w-full">
+          {pagedArticlesResult.isLoading ? (
+            <div className="px-4">
+              {Array(3)
                 .fill('')
                 .map((_, index) => (
-                  <div className={`flex flex-col md:flex-row justify-between py-2 px-4 md:pb-3 md:pt-3 ${index === 2 ? "" : "border-b"} border-gray-200 w-full animate-pulse`}>
+                  <div
+                    className={`flex flex-col w-full md:flex-row justify-between py-2 md:pb-3 md:pt-3 ${
+                      index === 2 ? '' : 'border-b'
+                    } border-gray-200 w-full animate-pulse`}
+                  >
                     <div className="h-14 my-2 md:my-0 md:h-6 md:w-full rounded-xs bg-gray-200"></div>
                   </div>
-                ))
-            : null}
+                ))}
+            </div>
+          ) : null}
           {pagedArticlesResult.isSuccess
             ? pagedArticlesResult.data?.content.map((article, index, array) => {
                 const isLast = index === array.length - 1;
