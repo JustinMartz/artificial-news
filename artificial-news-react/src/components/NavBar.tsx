4048895a44614b16ae84627de03ce7436dabf5cb
Justin Martz <justinmartz@protonmail.com>
2025-10-14 09:33:16 -0600

    [ui] Remove styled buttons

--- artificial-news-react/src/components/NavBar.tsx
+++ artificial-news-react/src/components/NavBar.tsx
@@ -1,18 +1,25 @@
+import { Link } from 'react-router';
 import { useGenerateArticle } from '../context/MutationProvider';
-import LinkButton from './LinkButton';
 
 const NavBar = () => {
-const { mutate, isPending } = useGenerateArticle();
-  
+  const { mutate, isPending } = useGenerateArticle();
+
+  let classNames: string;
+  if (isPending) {
+    classNames = "text-gray-400 bg-none p-0 border-none text-sm font-bold"
+  } else {
+    classNames = "text-blue-600 hover:underline bg-none p-0 border-none cursor-pointer text-sm font-bold"
+  }
+
   return (
     <div className="flex justify-around py-4">
-      <LinkButton
-        active={!isPending}
-        handleClick={mutate}
-        buttonText="Generate article"
-        small
-      />
-      <LinkButton active={true} buttonText="View articles" to="articles/" small />
+      <button
+        onClick={() => mutate()}
+        className={classNames}
+      >
+        GENERATE ARTICLE
+      </button>
+      <Link to="articles/"><span className="text-sm font-bold text-blue-600 hover:underline">VIEW ARTICLES</span></Link>
     </div>
   );
 };
